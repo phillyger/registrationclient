6bf4286a8ec9c73af9c52fe9520a3d1fc8616618
phillyger <ger@brilliantage.com>
2014-11-21 16:23:58 -0500

    Updated code to angular 1.3.4

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,7 +6,17 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-var app=angular.module('RegistrationClientApp', ['ionic', 'config', 'ui.router']);
+var app=angular.module('RegistrationClientApp',
+  [
+    'ionic',
+    'config',
+    'ui.router',
+    'http-auth-interceptor',
+    'ngMessages',
+    'ui.bootstrap',
+    'ui.mask',
+  ]
+);
 
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
